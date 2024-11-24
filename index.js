5ff97d8bf9ea5c1564bf693a4fc1bf0bca8b54d9
Saravia95 <53157860+Saravia95@users.noreply.github.com>
2024-11-23 21:18:46 -0500

    Renaming server.js to index.js

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.get("/api/languages", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server now running at http://localhost:${port}`);
-});
+module.exports = app;
+
+// app.listen(port, () => {
+//   console.log(`Server now running at http://localhost:${port}`);
+// });
